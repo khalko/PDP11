e55f1ac56f949d78904a1cef40e1f2e8d9cac43d
khalko <64355065+khalko@users.noreply.github.com>
2020-05-13 13:56:55 +0300

    Update load_file.c

--- load_file.c
+++ load_file.c
@@ -1,11 +1,20 @@
-void load_file() {
-	int i;
-	word w;
-	scanf("%hx %hx", pc, i);
-	while (i > 0) {
-		scanf("%hx", w);
-		w_write(pc, w);
-		pc += 2;
-		i--;
-	}
+void load_file(){
+    adr address;
+    unsigned short int n;
+    byte k = 0x00;
+    int i;
+    int j = 2;
+    while(1) {
+
+        j = scanf("%04hx%04hx", &address, &n);
+        //printf("%d\n", j);
+        if(j != 2){
+            break;
+        }
+        for (i = 0; i < n; i++) {
+            scanf("%02hhx", &k);
+            b_write(address + i, k);
+            //printf("scaned %d, i = %d, n = %d\n", k, i, n);
+        }
+    }
 }
